5a6b8cb1a6fdca1c46bc65585e8529c2e9b34af6
mars-convrtx <72063414+martiandev@users.noreply.github.com>
2023-10-18 01:24:31 +0800

    adding middlewares

--- be/src/app.js
+++ be/src/app.js
@@ -1,9 +1,56 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
 
+
+dotenv.config();
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send("hello");
 });
+
+//Morgan
+if(process.env.NODE_ENV!=="production"){
+    app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//parse json request url
+app.use(express.urlencoded({extended:true}));
+
+//parse json request body
+app.use(express.json());
+
+//sanitize request data
+app.use(mongoSanitize());
+
+//enable cookie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+
+//file upload
+app.use(fileUpload({
+    useTempFiles : true
+}));
+
+//corss middleware
+app.use(cors(
+    // {origin:'httpp://localhost:3000',}
+));
+
+
+
 
 export default app;
--- be/src/index.js
+++ be/src/index.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv';
 import app from './app.js';
 
 
-dotenv.config();
-
-const PORT = process.env.PORT || 8000;
-
+const PORT = process.env.PORT |8000 ;
+console.log(process.env.NODE_ENV)
 app.listen(PORT, ()=>{
     console.log(`Server is listening at ${PORT}`);
 });
