3ed71643a9c8a07b9bae6e59304ae1aff7ee9e6f
mars-convrtx <72063414+martiandev@users.noreply.github.com>
2023-10-18 02:54:37 +0800

    Setting up atlas db

--- be/src/index.js
+++ be/src/index.js
@@ -1,9 +1,58 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 import logger from './config/logger.config.js';
 
 
-const PORT = process.env.PORT |8000 ;
+const {MONGO_URL} = process.env;
+const {PORT} = process.env ;
 console.log(process.env.NODE_ENV)
-app.listen(PORT, ()=>{
+let server;
+
+server = app.listen(PORT, ()=>{
     logger.info(`Server is listening at ${PORT}`);
+    console.log(`process id`,process.pid);
+    // throw new Error("error");
+});
+
+//exit on mongodb error
+mongoose.connection.on('error',(err)=>{
+    logger.error(`mongodb connection error : ${error}`);
+    process.exit(1);
+});
+//mongodb debug mode
+if(process.env.NODE_ENV!=="production"){
+    mongoose.set('debug',true);
+}
+//mongodb connection
+mongoose.connect(MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    logger.info("Connected to mongodb");
+});
+
+
+
+//handle server error
+const exitHandler = () =>{
+    if(server){
+        logger.info("Server closed");
+        process.exit(1);
+    } else {
+        process.exit(1);
+    }
+}
+
+const unexpectedErrorHandler = (error)=>{
+    logger.error(error);
+    exitHandler();
+};
+
+process.on("uncaughtException",unexpectedErrorHandler);
+process.on("unhandledRejection",unexpectedErrorHandler);
+process.on("SIGTERM",()=>{
+    if(server){
+        logger.info("Server closed");
+        process.exit(1);
+    }
 });
---
+++ be/src/routes/auth.route.js
@@ -0,0 +1,14 @@
+
+import express from 'express';
+import { register,login,logout,refreshToken } from '../controllers/auth.controller.js';
+import trimRequest from 'trim-request';
+const router = express.Router();
+
+
+router.route('/register').post(trimRequest.all,register);
+router.route('/login').post(trimRequest.all,login);
+router.route('/logout').post(trimRequest.all,logout);
+router.route('/refreshToken').post(trimRequest.all,refreshToken);
+
+
+export default router;
