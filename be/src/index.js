eafecf719462328f1c33c0c31f88428a5ca0ec8a
mars-convrtx <72063414+martiandev@users.noreply.github.com>
2023-10-18 01:43:08 +0800

    adding logger

--- be/src/index.js
+++ be/src/index.js
@@ -1,8 +1,9 @@
 import app from './app.js';
+import logger from './config/logger.config.js';
 
 
 const PORT = process.env.PORT |8000 ;
 console.log(process.env.NODE_ENV)
 app.listen(PORT, ()=>{
-    console.log(`Server is listening at ${PORT}`);
+    logger.info(`Server is listening at ${PORT}`);
 });
